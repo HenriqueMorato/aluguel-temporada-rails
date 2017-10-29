acdacc3fe4c0f18f37dceb497945fa0803c05caa
HenriqueMorato <riquemorato@gmail.com>
2017-10-29 19:54:32 -0200

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: {
+    message: 'Você deve informar o primeiro nome'
+  }
+
+  validates :last_name, presence: {
+    message: 'Você deve informar o ultimo nome'
+  }
+
+  validates :cpf, presence: {
+    message: 'Você deve informar o cpf'
+  }
 end
